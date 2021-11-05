2dbaafe7080d53f26239cb07e1f7da1baa249d67
Artem Nikolaev <anikolaev82@yandex.ru>
2021-11-05 18:55:36 +0300

    Modifed algoritm generation passwords

--- abstract/AbcGeneretorPassword.py
+++ abstract/AbcGeneretorPassword.py
@@ -1,37 +1,33 @@
 from templates_symbols.template_letters import TemplateSymbols
 from abc import ABC
 from inspect import isclass
-from random import shuffle, randint
+from random import shuffle, randint, choice
 
 
 class AbcGenPass(ABC):
     __symb_pass = {}
 
     def __init__(self, length_pswd: int = 6,  **kwargs):
+        if length_pswd < len(kwargs):
+            length_pswd = len(kwargs)
         self.__length_pswd = length_pswd
         for v in kwargs.values():
             if isclass(v) and issubclass(v, TemplateSymbols):
                 self.__symb_pass.setdefault(v, v())
 
     def __get_template_password(self):
-        len_cls = len(self.__symb_pass)
-        count_ever_cls = self.__length_pswd // len_cls
-        odd = self.__length_pswd - count_ever_cls * len_cls
-        template_password = [count_ever_cls for i in range(len_cls)]
-        for _ in range(odd):
-            template_password[randint(0, len_cls - 1)] += 1
+        values = list(self.__symb_pass.values())
+        template_password = values
+        while len(template_password) < self.__length_pswd:
+            template_password.append(choice(values))
+            shuffle(template_password)
         return template_password
 
     def make_password(self):
         new_password = []
         template_password = self.__get_template_password()
-        try:
-            for i, cls in enumerate(self.__symb_pass.values()):
-                new_password.extend(cls.get_random(template_password[i]))
-            shuffle(new_password)
-            return ''.join(new_password)
-        except ZeroDivisionError:
-            print("Не выбраны символы для пароля")
-
-
+        for i in template_password:
+            new_password.extend(i.get_random())
+        shuffle(new_password)
+        return ''.join(new_password)
 
